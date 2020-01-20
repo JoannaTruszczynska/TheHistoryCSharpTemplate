70d9e19081f1882c14ec90243fb8f854dfcc9d4a
Joanna Truszczyńska <joanna.truszczynska@gmail.com>
2020-01-20 12:48:08 +0100

    function add on array

--- src/Codecool.TheHistory/TheHistoryArray.cs
+++ src/Codecool.TheHistory/TheHistoryArray.cs
@@ -1,26 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
 
 namespace Codecool.TheHistory
 {
-    public class TheHistoryArray : TheHistoryAbstract
+   public class TheHistoryArray : TheHistoryAbstract
     {
         /// <summary>
         /// This implementation should use a string Array, so don't change that!
         /// </summary>
         private string[] _wordsArray = new string[0];
 
+        private string[] _split;
         /// <summary>
         /// Don't change this either. It's needed for testing.
         /// </summary>
         public override IEnumerable<string> WordsList => _wordsArray;
-        
-        public override int Size => throw new NotImplementedException();
 
+        public override int Size => _split.Length;
+        
         public override void Add(string text)
         {
             // TODO: Check the ITheHistory interface for more information
-            throw new NotImplementedException();
+
+            string textTemp = null;
+            int index = 0;
+            while (index < text.Length)
+            {
+                if (!(text[index].ToString().Equals(" ")|text[index].ToString().Equals("\n")|text[index].ToString().Equals("\t")))
+
+                {
+                    textTemp = textTemp + text[index];
+                    index += 1;
+                }
+                else
+                {
+                    int next = 1;
+                    while (text[index+next].ToString().Equals(" ")|text[index+next].ToString().Equals("\n")|text[index+next].ToString().Equals("\t"))
+                    {
+                        next += 1;
+                    }
+
+                    textTemp = textTemp + text[index];
+                    index = index + next;
+                }
+                
+            }
+            _split = textTemp.Split(new char[] { ' ', '\n', '\t' });
+            _wordsArray = _wordsArray.Concat(_split).ToArray();
+
+            foreach (var VARIABLE in _wordsArray)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            
+
+
+          
+
+            //string[] split = text.Split(new char[] { ' ', '\n', '\t' });
+
+            //foreach (var word in split)
+            //{
+            //    if (word.Contains(' '))
+            //    {
+                    
+            //    }
+            //}
+            
+            
+            
+            //
+
+            //foreach (var VARIABLE in split)
+            //{
+            //    if (VARIABLE.Contains(' ') | VARIABLE.Contains('\n') | VARIABLE.Contains('\t'))
+            //    {
+            //        VARIABLE.Remove();
+            //    }
+            //}
+           
+            //_wordsArray = _wordsArray.Concat(split).ToArray();
+            //foreach (var VARIABLE in _wordsArray)
+            //{
+            //     Console.WriteLine(VARIABLE);
+            //}
+           
+            
         }
 
         public override void Clear()
